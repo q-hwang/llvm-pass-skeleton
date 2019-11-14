f473df873f7031cdb4cb24cece0fe3714ccfaf0a
q-hwang <qh53@cornell.edu>
2019-11-13 23:20:48 -0500

    parsec

--- skeleton/Skeleton.cpp
+++ skeleton/Skeleton.cpp
@@ -4,11 +4,13 @@
 #include "llvm/IR/LegacyPassManager.h"
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
 #include "llvm/IR/Instruction.h"
+#include "llvm/IR/Instructions.h"
 #include "llvm/IR/BasicBlock.h"
 #include "llvm/IR/Metadata.h"
 #include <map>
 #include <list> 
 #include "llvm/IR/Constants.h"
+
 using namespace llvm;
 
 namespace {
@@ -16,9 +18,10 @@ namespace {
     static char ID;
     SkeletonPass() : FunctionPass(ID) {}
 
-    void merge_chains(BasicBlock *start, BasicBlock *end, std::map<BasicBlock*, BasicBlock*>* chains, std::map<BasicBlock*, bool> * start_blocks){
+    void merge_chains(BasicBlock *fb, BasicBlock *start, BasicBlock *end, std::map<BasicBlock*, BasicBlock*>* chains, std::map<BasicBlock*, bool> * start_blocks){
    
-        if((*start_blocks)[end] && (*chains)[start] == start) {
+        if((*start_blocks)[end] && (*chains)[start] == start && end != fb) {
+
             // errs() << "merge start " << *start << "!\n";
             // errs() << "merge end " << *end << "!\n";
            (*start_blocks)[end] = false;
@@ -31,68 +34,87 @@ namespace {
         std::map<BasicBlock*, bool> start_blocks;
         std::vector<std::tuple<int,  BasicBlock*,BasicBlock*> > prof_edges;
         std::vector<std::tuple<BasicBlock*,BasicBlock*> > non_profedges;
+        std::vector<BasicBlock*> v_blocks;
+        bool first = true;
+        BasicBlock* fb  ;
         for (auto &B: F) {
           BasicBlock *b = dyn_cast <BasicBlock> (&B);
+          if (first) {
+
+            first = false;
+            fb = b;
+          }
           chains[b] = b; 
           start_blocks[b] = true; 
+          v_blocks.push_back(b);
           Instruction *terminator = b -> getTerminator();
           
           BasicBlock * parent = terminator -> getParent();
           MDNode * prof_n = terminator -> getMetadata("prof") ;
           uint n = terminator -> getNumSuccessors();
-          if (terminator -> hasMetadata() and prof_n and (prof_n->getNumOperands() == n + 1)){
+          if (terminator -> hasMetadata() and prof_n and (prof_n->getNumOperands() == n+1)){
             for(int i =0; i < n ; i ++ ) {
               uint weight =  mdconst::dyn_extract<ConstantInt>(prof_n -> getOperand(i+1))->getZExtValue();
-              
-              prof_edges.push_back({i, parent, terminator-> getSuccessor(i)});
+              if (dyn_cast <BranchInst> (terminator) or dyn_cast <SwitchInst> (terminator)) {
+                prof_edges.push_back({i, parent, terminator-> getSuccessor(i)});
+              }
             }
-          } else if(terminator -> getNumSuccessors () > 0) {
+          } else if(dyn_cast <BranchInst> (terminator) and terminator -> getNumSuccessors () == 1) {
             non_profedges.push_back({parent, terminator-> getSuccessor(0)});
           }      
         }
-        sort(prof_edges.begin(), prof_edges.end());
+        std::sort(prof_edges.begin(), prof_edges.end());
+
 
         // merge
   
         for(int i = 0; i < non_profedges.size() ; i ++ ) {
           BasicBlock *start = std::get<0>(non_profedges.at(i));
           BasicBlock *end = std::get<1>(non_profedges.at(i));
-          merge_chains(start, end, &chains, &start_blocks);
+          merge_chains(fb, start, end, &chains, &start_blocks);
         }
 
         for(int i = prof_edges.size() - 1; i >= 0 ; i --) {
           // traversing edges in descending freq order
           BasicBlock *start = std::get<1>(prof_edges.at(i));
           BasicBlock *end = std::get<2>(prof_edges.at(i));
-          merge_chains(start, end, &chains, &start_blocks);
+          merge_chains(fb,start, end, &chains, &start_blocks);
         }
+        
         
         // final order
         // traversing chains
         Function::BasicBlockListType &all_blocks = F.getBasicBlockList();
         Function::iterator InsertPos = F.begin();
         unsigned moved = 0;
-        for (auto &B: F) {
-          BasicBlock *b = dyn_cast <BasicBlock> (&B);
+        bool start = true;
+   
+        std::vector<BasicBlock*>::iterator b_it =v_blocks.begin();
+        while (b_it != v_blocks.end()) {
+
+          BasicBlock *b = *b_it;
+
           if(start_blocks[b]) {
             // start of a chain
+ 
             BasicBlock *curr = b; 
-            while (true) {                
+            while (true) {             
                 if (&*InsertPos != curr) {
-                  all_blocks.splice(InsertPos, all_blocks, curr);
+                   all_blocks.splice(InsertPos, all_blocks, curr);
                   moved += 1;
+                } else{
+                  InsertPos ++;
                 }
-                InsertPos++;
+                start_blocks[curr] = false;
+               
                 BasicBlock *next = chains[curr];
                 if(curr == next) break;
                 curr = next;
             }
           }
+          b_it ++;
         }
-        // for (auto &B: F) {
-        //   errs() << "final blocks " << B << "!\n";
-        // }
-        
+
         return moved > 0;
     }
   };
@@ -103,4 +125,3 @@ char SkeletonPass::ID = 0;
 
 // Register the pass so `opt -skeleton` runs it.
 static RegisterPass<SkeletonPass> X("skeleton", "a useless pass");
-
