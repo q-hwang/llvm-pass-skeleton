7f92c8752fd1e03a30f389bd858dea0d1eda4d98
q-hwang <qh53@cornell.edu>
2019-11-24 22:11:47 -0500

    script

---
+++ parsec-3.0/bin/clang-opt.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+echo "You provided the arguments:" "$@"
+
+COMPILER="/usr/bin/clang++-9"
+ARGS=""
+while [ "$1" != "" ]; do
+    case $1 in
+        -c)
+            FILE=$2
+            shift
+            ;;
+        -o)
+            OUT=$2
+            shift
+            ;;
+        *)
+            ARGS=$ARGS" "$1
+            ;;
+    esac
+    shift
+done
+if [ -z "$OUT" ]
+then
+    if [ -z "$FILE" ]
+    then 
+        OUT='a.out'
+    else
+        OUT=${FILE%.*}.o
+    fi
+fi
+
+
+if [ -z "$FILE" ]
+then
+    echo "$COMPILER $ARGS -o $OUT"
+    eval $COMPILER $ARGS -o $OUT 
+else
+    echo "$COMPILER $ARGS -S -emit-llvm $FILE -o ${FILE%.*}.ll"
+	eval "$COMPILER $ARGS -S -emit-llvm $FILE -o ${FILE%.*}.ll"
+    echo "opt-9 --gvn  -S ${FILE%.*}.ll -ogit  ${FILE%.*}_.ll"
+	eval "opt-9 --gvn  -S ${FILE%.*}.ll -o ${FILE%.*}_.ll"
+    echo "$COMPILER $ARGS -c ${FILE%.*}_.ll -o $OUT"
+	eval "$COMPILER $ARGS -c ${FILE%.*}_.ll -o $OUT"
+fi
