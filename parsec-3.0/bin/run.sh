75a9e147e9f28401500d170af83753d0090c21f8
q-hwang <qh53@cornell.edu>
2019-12-17 23:33:02 -0500

    parse output

--- parsec-3.0/bin/run.sh
+++ parsec-3.0/bin/run.sh
@@ -1,20 +1,43 @@
 #!/bin/bash
 BUILD_DIR="/home/qian/llvm-pass-skeleton/parsec-3.0/pkgs/kernels/streamcluster/inst/amd64-linux.gcc"
-for i in {0..0}
+comb=50
+for i in `seq 1 $comb`;
 do
+    echo "$i"
     if [ -d "$BUILD_DIR" ] 
     then
-        echo 'rm build dir'
+        #echo 'rm build dir'
         rm -r "$BUILD_DIR"
     fi
-    cat opt_all.txt | python3.7 order_gen.py 10 > opt.txt
+    cat opt_all.txt | python3.7 order_gen.py 270 > opt.txt
 
     parsecmgmt -a build -c gcc -p streamcluster > "/dev/null"
-
-    for j in {0..5}
+    n=10
+    f_all=0.0
+    u_all=0.0
+    s_all=0.0
+    for j in `seq 1 $n`;
     do
-        echo "$j"
-        parsecmgmt -a run -c gcc -p streamcluster -i simlarge
+        x=$(parsecmgmt -a run -c gcc -p streamcluster -i simlarge | grep 'real\|user\|sys')
+        f=$(echo "$x" | grep 'real')
+        u=$(echo "$x" | grep 'user')
+        s=$(echo "$x" | grep 'sys')
+        f="${f#*m}"
+        u="${u#*m}"
+        s="${s#*m}"
+        f="${f::-1}"
+        u="${u::-1}"
+        s="${s::-1}"
+        f_all=$(echo "$f_all + $f" | bc) 
+        u_all=$(echo "$u_all + $u" | bc) 
+        s_all=$(echo "$s_all + $s" | bc) 
     done
-    mv opt.txt opts/opt_$(date +"%T").txt
+    
+    file="opts/opt_$(date +"%T").txt"
+    mv opt.txt "$file"
+    f_all=$(echo "$f_all / $n" | bc -l)
+    u_all=$(echo "$u_all / $n" | bc -l)
+    s_all=$(echo "$s_all / $n" | bc -l)
+    echo "$f_all $u_all $s_all"
+    echo "$f_all $u_all $s_all" >> "$file"
 done
